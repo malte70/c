e9bd5517d836b0306adcb4ac760a2ec6ac8b74c1
Malte Bublitz <malte70@mcbx.de>
2015-06-22 17:20:45 +0200

    file_info now displays an error message if stat fails

--- src/file_info.c
+++ src/file_info.c
@@ -1,15 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 int main(int argc, char **argv) {
+	char msg[200] = "";
+	
 	if(argc != 2)
 		return 1;
 	
 	struct stat fileStat;
-	if(lstat(argv[1],&fileStat) < 0)    
+	if(lstat(argv[1],&fileStat) < 0) {
+		strcpy(msg, argv[0]);
+		strcat(msg, ": Cannon stat \"");
+		strcat(msg, argv[1]);
+		strcat(msg, "\"");
+		perror(msg);
 		return 1;
+	}
 	
 	printf("Information for %s\n",argv[1]);
 	printf("---------------------------\n");
