e35e88beceb44123cda4dba244d9e55b52538148
Malte Bublitz <malte70@kosmos.mcbx.de>
2015-10-17 13:51:19 +0200

    Now working on FreeBSD

--- configure.inc.sh
+++ configure.inc.sh
@@ -2,6 +2,9 @@
 # configure.inc.sh
 #
 
+CC=
+SED=
+
 c_welcome() {
 	cat <<EOF
 
@@ -15,20 +18,15 @@ c_check_os() {
 	printf "Checking for operating system... "
 	_OS=$(uname -o 2>/dev/null || uname -s)
 	echo $_OS
-	if [[ $_OS != "GNU/Linux" ]]
-	then
-		echo "$0: Error: Unsupported operating system: $_OS!" >&2
-		exit 1
-	fi
 }
 c_check_arch() {
-	echo -n "Checking for CPU architecture... "
+	printf "Checking for CPU architecture... "
 	_CPUTYPE=$(uname -m)
 	echo $_CPUTYPE
 }
 
 c_check_cc() {
-	echo -n "Checking for C compiler... "
+	printf "Checking for C compiler... "
 
 	if which gcc &>/dev/null
 	then
@@ -38,6 +36,26 @@ c_check_cc() {
 	then
 		CC=$(which cc)
 		echo $CC
+	elif which clang &>/dev/null
+	then
+		CC=$(which clang)
+		echo $CC
+	else
+		echo "Not found!"
+		exit 1
+	fi
+}
+c_check_gsed() {
+	printf "Checking for GNU sed... "
+	
+	if sed --version 2>/dev/null | head -n1 | grep GNU &>/dev/null
+	then
+		SED=$(which sed)
+		echo $SED
+	elif gsed --version 2>/dev/null | head -n1 | grep GNU &>/dev/null
+	then
+		SED=$(which gsed)
+		echo $SED
 	else
 		echo "Not found!"
 		exit 1
@@ -46,13 +64,13 @@ c_check_cc() {
 
 c_check_header() {
 	header=$1
-	echo -n "Checking for $header... "
+	printf "Checking for $header... "
 	cat <<EOF >/tmp/$$_test.c
 #include <$header>
 
 int main(){return 0;}
 EOF
-	gcc -o /tmp/$$_test /tmp/$$_test.c &>/dev/null
+	$CC -o /tmp/$$_test /tmp/$$_test.c &>/dev/null
 	if [[ $? -eq 0 ]]
 	then
 		rm -f /tmp/$$_test /tmp/$$_test.c
@@ -72,21 +90,21 @@ c_gen_makefile() {
 		echo "CFLAGS=-Wall -march=native -O3"
 		echo ""
 		echo -n "all: "
-		grep : Makefile.in | cut -d: -f1 | sed 's@^@bin/@'| tr "\n" " " | sed 's/ $/\xA	\x0A/'
+		grep : Makefile.in | cut -d: -f1 | $SED 's@^@bin/@'| tr "\n" " " | $SED 's/ $/\xA	\x0A/'
 		
 		for binary in $(grep : Makefile.in | cut -d: -f1)
 		do
 			echo -n "bin/$binary: "
-			grep "$binary:" Makefile.in | cut -d: -f2 | tr " " "\n" | sed 's@^@src/@;s@\.c@\.o@g' | grep '.o$' | tr "\n" " " | sed 's/ $/\x0A/'
+			grep "$binary:" Makefile.in | cut -d: -f2 | tr " " "\n" | $SED 's@^@src/@;s@\.c@\.o@g' | grep '.o$' | tr "\n" " " | $SED 's/ $/\x0A/'
 			echo -e "\t@echo \"[LD]    bin/$binary\""
 			echo -n -e "\t@\$(CC) \$(CFLAGS) "
 			sources=$(grep "^$binary:" Makefile.in | cut -d: -f2)
-			for f in $(echo $sources | tr " " "\n" | grep '.c' | sed 's@^@src/@')
+			for f in $(echo $sources | tr " " "\n" | grep '.c' | $SED 's@^@src/@')
 			do
 				grep 'math.h' $f &>/dev/null && echo -n "-lm "
 			done
 			echo -n "-o bin/$binary "
-			echo $sources | tr " " "\n" | sed 's@^@src/@;s@\.c@.o@g' | grep ".o$" | tr "\n" " "
+			echo $sources | tr " " "\n" | $SED 's@^@src/@;s@\.c@.o@g' | grep ".o$" | tr "\n" " "
 			echo -e "\n\t"
 		done
 		
